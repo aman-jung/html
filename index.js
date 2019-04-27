358ee7b0bc6927a02f1e8397db0f1c3e8a356f29
aman-tunerlabs <aman@tunerlabs.com>
2019-04-27 22:23:35 +0530

    addd more

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+
+app.get("/",(req,res)=>{
+    console.log("HELLO WORLD");
+})
 app.listen(process.env.PORT || 5000,()=>{
 	console.log("server is up and running");
 });
