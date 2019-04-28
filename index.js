ad3f848c3292655787a672a4658c567280b01d43
aman-tunerlabs <aman@tunerlabs.com>
2019-04-29 00:29:26 +0530

    code optimize

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ var app = express();
 app.get("/",(req,res)=>{
     console.log("HELLO WORLD");
 })
+
+function handle(signal) {
+    console.log(`Received ${signal}`);
+  }
+  
+process.on('SIGINT', handle);
+process.on('SIGTERM', handle);
+  
 app.listen(process.env.PORT || 5000,()=>{
 	console.log("server is up and running");
 });
