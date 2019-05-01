dca1cbc71f9ad8194ae15a016f4565c38eb7464e
aman-tunerlabs <aman@tunerlabs.com>
2019-05-01 21:11:34 +0530

    added index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get("/",(req,res)=>{
-    console.log("HELLO WORLD");
+  res.send("Hello world")
 })
   
 let server = app.listen(process.env.PORT || 5000,()=>{
