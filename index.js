dd87509ac6670974a41adb6102ea6ebf8e89cca9
aman-tunerlabs <aman@tunerlabs.com>
2019-05-01 20:53:23 +0530

    added port

--- index.js
+++ index.js
@@ -5,26 +5,8 @@ var app = express();
 app.get("/",(req,res)=>{
     console.log("HELLO WORLD");
 })
-
-process
-  .on('SIGTERM', shutdown('SIGTERM'))
-  .on('SIGINT', shutdown('SIGINT'))
-  .on('uncaughtException', shutdown('uncaughtException'))
-  .on('SIGKILL',shutdown('SIGKILL'));
-
-setInterval(console.log.bind(console, 'tick'), 1000);
   
-app.listen(process.env.PORT || 5000,()=>{
-	console.log("server is up and running");
+let server = app.listen(process.env.PORT || 5000,()=>{
+  let port = server.address().port;
+	console.log("server is up and running",port);
 });
-
-function shutdown(signal) {
-    return (err) => {
-      console.log(`${ signal }...`);
-      if (err) console.error(err.stack || err);
-      setTimeout(() => {
-        console.log('...waited 5s, exiting.');
-        process.exit(err ? 1 : 0);
-      }, 5000).unref();
-    };
-  }
